5853b95b4f39d52cf24de06821fe9c2685990cd1
elBichon <reuglewiczjeanedouard@gmail.com>
2021-04-10 00:50:01 +0200

    main function

--- app.py
+++ app.py
@@ -44,41 +44,7 @@ rcnn = MaskRCNN(mode='inference', model_dir='.', config=config_files.TestConfig(
 rcnn.load_weights('mask_rcnn_fashion_0003.h5', by_name=True)
 
 
-def main():
-	option = st.selectbox('What are you looking for ?',('top, t-shirt, sweatshirt', 'pants', 'shorts', 'skirt', 'dress', 'hat', 'shoe', 'bag, wallet'))
-	search_cloth = app_utils.select_cloth(option)
-	st.title("Fashion AI")
-	st.subheader("Your clothes research vision assistant")
-	image_file = st.sidebar.file_uploader("Upload Image",type=['png','jpeg','jpg'])
 
-	if image_file is not None:
-		st.image(image_file,width=250,height=250)
-		image = np.array(Image.open(image_file))
-		img = app_utils.img_enhance(image)
-		img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_AREA)  
-		img2 = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_AREA)  
-		results = rcnn.detect([img], verbose=0)
-		r = results[0]
-		
-
-		class_id = list(r['class_ids'])
-		i = 0
-		while i < len(class_id):
-			class_id[i] = class_id[i]-1
-			i += 1
-
-		if search_cloth in class_id:
-			cloth_to_search = class_id.index(search_cloth)
-		else:
-			pass
-
-		r['class_ids'] = np.array(class_id)
-		mask = r['masks']
-		mask = mask.astype(int)
-		blur = app_utils.extract_from_mask(r, img, img2, mask, cloth_to_search)
-		image = img2[r['rois'][cloth_to_search][0]:r['rois'][cloth_to_search][2], r['rois'][cloth_to_search][1]:r['rois'][cloth_to_search][3]]
-		dst = app_utils.get_mask(blur, image)
-		st.image(dst,width=250,height=250)
 
 if __name__ == '__main__':
-	main()
+	app_utils.main(rcnn, IMAGE_SIZE)
